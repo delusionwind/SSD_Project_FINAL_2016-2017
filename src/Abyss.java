d32d20ffa9fb887d9bfd140bfa976194bbdc6db0
Napon Kittisiriprasert <speedojays@gmail.com>
2017-05-26 10:00:29 +0700

    modified

--- src/Abyss.java
+++ src/Abyss.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import javax.swing.JFrame;
 
 public class Abyss extends JFrame{
-
+	
 }
-=======
-import javax.swing.JFrame;
-
-public class Abyss extends JFrame{
-
-}
->>>>>>> origin/newBranch
--- src/Home.java
+++ src/Home.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -28,34 +27,3 @@ public class Home extends JFrame{
 	}
 
 }
-=======
-import java.awt.Dimension;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-import javax.swing.JFrame;
-
-public class Home extends JFrame{
-	
-	public static final int HEIGHT = 900, WIDTH = 600;
-
-	public Home() {
-		initHomeData();
-		this.setPreferredSize(new Dimension(HEIGHT,WIDTH));
-		pack();
-		
-	}
-	
-	private void initHomeData() {
-		
-	}
-	
-	public static void main(String[] args) {
-		Home home = new Home();
-		home.setVisible(true);
-		home.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-	}
-
-}
->>>>>>> origin/newBranch
